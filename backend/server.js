706d6e2780d913ade3bf15f5af80fc337cd66591
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-12 18:13:10 +0100

    Change in .env and consequentially in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,15 @@ app.use(express.json());
 app.use(tracksRoutes)
 
 
-mongoose.connect('mongodb+srv://dede:<password>@mern-deploy-render.ugrdoha.mongodb.net/?retryWrites=true&w=majority')
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT, () => console.log(`Connected to DB and Listening on port ${process.env.PORT}`));
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 
 
 
