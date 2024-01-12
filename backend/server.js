0c5e956119bd61c28c7db29428c08cab31e4d23f
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-12 16:14:49 +0100

    Provato con mongoose.connect(...<PASSWORD>

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,8 @@ app.use(express.json());
 app.use(tracksRoutes)
 
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        app.listen(process.env.PORT, () => console.log(`Connected to DB and Listening on port ${process.env.PORT}`));
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+mongoose.connect('mongodb+srv://dede:<password>@mern-deploy-render.ugrdoha.mongodb.net/?retryWrites=true&w=majority')
+
 
 
 app.use('/api/tracks', tracksRoutes)
