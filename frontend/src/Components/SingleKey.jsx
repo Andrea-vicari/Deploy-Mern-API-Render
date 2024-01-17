e0eafd25ba6d349aa9739814da2a49ceabca32c8
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-17 17:00:32 +0100

    Added a TrackUrl console.log

--- frontend/src/Components/SingleKey.jsx
+++ frontend/src/Components/SingleKey.jsx
@@ -49,7 +49,7 @@ function SingleKey (){
       }
 
 
-    const playSound = (i, trackName, trackID) => {
+    const playSound = (i, trackName, trackID, trackUrl) => {
 
         document.getElementById(i+300).classList.remove("d-none");
         document.getElementById(i+200).classList.add("playing");
@@ -60,6 +60,8 @@ function SingleKey (){
 
         let mp3_toplay = document.getElementById(trackID);
         console.log(mp3_toplay)
+
+        console.log(trackUrl)
        // mp3_toplay.loop = true;
         mp3_toplay.play();
         var MP3_duration = document.getElementById(trackID).duration;
@@ -73,7 +75,7 @@ function SingleKey (){
     return(
         data.map((d, i) => (
             <div key={d.id} id={100+i}>
-                <button id={200+i} className="btn btn-success active btn-sq-responsive" onClick={() => playSound(i, d.trackName, d.id)}>
+                <button id={200+i} className="btn btn-success active btn-sq-responsive" onClick={() => playSound(i, d.trackName, d.id, d.trackUrl)}>
                 <span id={i+300} className="spinner-border d-none spinner-border-sm" aria-hidden="true"></span>
                 <audio className="clip" id={d.trackName} src={d.trackUrl}></audio>
                 </button>
