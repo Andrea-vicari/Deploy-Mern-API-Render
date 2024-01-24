73fe25a1a9093b28a62f6070b15fbd6053c376f2
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-24 14:08:39 +0100

    Re added the CRUD operation in the API/tracks

--- backend/routes/tracks.js
+++ backend/routes/tracks.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
-const {viewAllTracks} = require('../controllers/trackController');
+const {createNewTrack, viewAllTracks, getSingleTrack, deleteTrack, updateTrack} = require('../controllers/trackController');
 
 const router = express.Router();
 
 router.get('/', viewAllTracks);
+
+// Get Single
+router.get('/:id', getSingleTrack);
+
+// POST a new track
+router.post('/', createNewTrack);
+
+// DELETE Single
+router.delete('/:id', deleteTrack);
+
+// UPDATE Single
+router.patch('/:id', updateTrack);
 
 module.exports = router;
