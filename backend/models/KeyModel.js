1e647dcaeaade45ce351ab82d5fee9553d3c6fc7
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-25 08:55:58 +0100

    added user name to the KEyModel schema

--- backend/models/KeyModel.js
+++ backend/models/KeyModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const keySchema = new mongoose.Schema({
+        userName:{
+        type: String,
+        required: true
+        },
         keyNumber:{
         type: Number,
         required: true
--- frontend/src/Components/Update.jsx
+++ frontend/src/Components/Update.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react'
 import { useEffect , useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -6,52 +5,56 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 function Update (){
 
+    const [mp3Url, setMp3URL] = useState([]);
 
+    const makeAPICall = async () => {
+        try {
+          const response = await fetch('https://deploy-mern-api-render.vercel.app/api/tracks/', {mode:'cors'});
+          const data = await response.json();
+          setMp3URL(data)
+          console.log(data)
+
+        }
+        catch (e) {
+          console.log(e)
+        }
+      }
+      useEffect(() => {
+        makeAPICall();
+      }, [])
 
     const {id} = useParams();
 
-    const [inputData, setInputData] = useState({
-        id: id,
-        trackURL: '',
 
-    })
     const navigate = useNavigate();
 
-    useEffect(() => {
-        axios.get('https://deploy-mern-api-render.vercel.app/api/tracks/')
-        .then(res => setInputData(res.data))
-        .then(console.log(inputData))
-        .catch(err => console.log(err))
-    }, [])
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        axios.put('https://deploy-mern-api-render.vercel.app/api/tracks/'+id , inputData)
-        .then(res => {
-            alert("Data Updated Successfully!")
-            navigate('/')
-        })
-    }
+      // Here the Crud operation
+
 
 
     return(
         <main className="container">
-                <form onSubmit={handleSubmit}>
 
                     <h1 className="h3 mb-3 text-white">Edit: {id}</h1>
                     <p className="text-white">Choose the Track for this key</p>
-                    <div className="form-floating">
 
-
-                    <select className="form-select mb-2" aria-label="Default select example">
-
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                    </div>
+                    {mp3Url.map((e)=>{
+                  return (
+                    <div className="col-3 mb-2" key={e._id}>
+                    <div className="card">
+                        <h5 className="card-header">TrackName: {e.trackName}</h5>
+                        <div className="card-body">
+                        <audio className="clip" id={e.trackName} src={e.trackUrl}></audio>
+                        </div>
+                      </div>
                     <button className="btn btn-primary w-100 py-2" type="submit">Confirm</button>
+                    </div>
+                    );})}
 
-                </form>
+
+
+
         </main>
     )
 
