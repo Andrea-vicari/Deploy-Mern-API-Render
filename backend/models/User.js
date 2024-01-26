0c43ec012f9ba2e4d5104cfc31d04b345827d617
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-26 15:23:23 +0100

    Restarted server side with TutNETNin

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String,
-    role: {
-        type: String,
-        default: "visitor"
-    }
+    password: String
+
 })
 
 const UserModel = mongoose.model("users", UserSchema)
