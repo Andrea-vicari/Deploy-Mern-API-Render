2c8034bfe81d77cc3508a167e66c6854729d77d0
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-26 14:53:36 +0100

    On going

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const cookieParser = require('cookie-parser')
 require ('dotenv').config();
 const app = express();
 
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 app.use(express.json());
 
 // !!! Important to avoid CORS block (!! Check the final slash / )
@@ -27,17 +30,71 @@ mongoose.connect(process.env.MONGO_DB_CONNECT_URI)
         console.log(error)
     })
 
+    const varifyUser = (req, res, next) => {
+        const token = req.cookies.token;
+        if(!token) {
+            return res.json("Token is missing")
+        } else {
+            jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+                if(err) {
+                    return res.json("Error with token")
+                } else {
+                    if(decoded.role === "admin") {
+                        next()
+                    } else {
+                        return res.json("not admin")
+                    }
+                }
+            })
+        }
+    }
+
+    app.get('/dashboard',varifyUser ,(req, res) => {
+        res.json("Success")
+    })
+
+    app.post('/register', (req, res) => {
+        const {name, email, password} = req.body;
+        bcrypt.hash(password, 10)
+        .then(hash => {
+            UserModel.create({name, email, password: hash})
+            .then(user => res.json("Success"))
+            .catch(err => res.json(err))
+        }).catch(err => res.json(err))
+    })
+
+    app.post('/login', (req, res) => {
+        const {email, password} = req.body;
+        UserModel.findOne({email: email})
+        .then(user => {
+            if(user) {
+                bcrypt.compare(password, user.password, (err, response) => {
+                    if(response) {
+                      const token = jwt.sign({email: user.email, role: user.role},
+                            "jwt-secret-key", {expiresIn: '1d'})
+                        res.cookie('token', token)
+                        return res.json({Status: "Success", role: user.role})
+                    }else {
+                        return res.json("The password is incorrect")
+                    }
+                })
+            } else {
+                return res.json("No record existed")
+            }
+        })
+    })
+
 
 const tracksRoutes = require('./routes/tracks');
 const keysRoutes = require('./routes/keys');
-const usersRoutes = require('./routes/users');
+
 
 
 app.use(tracksRoutes)
 app.use(keysRoutes)
-app.use(usersRoutes)
+
 
 app.use('/api/tracks', tracksRoutes)
 app.use('/api/keys', keysRoutes)
-app.use('/api/users', usersRoutes)
+
 
