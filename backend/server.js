5b057d19e16737745fead9ba950c0bbb179ca614
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-26 09:22:27 +0100

    Added UserModel and auth and user path

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,16 @@ mongoose.connect(process.env.MONGO_DB_CONNECT_URI)
 
 const tracksRoutes = require('./routes/tracks');
 const keysRoutes = require('./routes/keys');
+const usersRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
 
 
 app.use(tracksRoutes)
 app.use(keysRoutes)
-
+app.use(usersRoutes)
+app.use(authRoutes)
 
 app.use('/api/tracks', tracksRoutes)
 app.use('/api/keys', keysRoutes)
+app.use('/api/users', usersRoutes)
+app.use('/api/auth', authRoutes)
