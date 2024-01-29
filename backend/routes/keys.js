cca67f5fedd99f4e4bff9921bbaa67f11986638e
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-29 10:21:08 +0100

    Added the middle ware func only to the keys routes

--- backend/routes/keys.js
+++ backend/routes/keys.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const {createNewKey, viewAllKeys, getSingleKey, deleteKey, updateKey} = require('../controllers/KeyController');
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router();
-
+router.use(requireAuth)
 
 router.get('/', viewAllKeys);
 
