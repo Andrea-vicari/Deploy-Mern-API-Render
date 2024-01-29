17389e18a58447da92b2f0ecf5c4fe226070982b
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-29 10:59:36 +0100

    Ok

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require ('dotenv').config();
 const app = express();
 
+
 app.use(express.json());
 
 app.use((req,res, next)=>{
@@ -20,7 +21,6 @@ app.use(cors(
         credentials: true
     }
 ));
-
 
 
 mongoose.connect(process.env.MONGO_DB_CONNECT_URI)
