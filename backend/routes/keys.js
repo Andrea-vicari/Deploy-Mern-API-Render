a5ff3e7ba470031b4b220a5bcc3b5df3d95b37f2
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-29 09:56:49 +0100

    Added middleware to backend

--- backend/routes/keys.js
+++ backend/routes/keys.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const {createNewKey, viewAllKeys, getSingleKey, deleteKey, updateKey} = require('../controllers/KeyController');
 
+const requireAuth = require('../middleware/requireAuth')
+
 const router = express.Router();
+
+router.use(requireAuth)
 
 router.get('/', viewAllKeys);
 
