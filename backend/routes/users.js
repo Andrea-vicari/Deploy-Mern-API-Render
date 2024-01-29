9c3c683bd120b1d5fdf8e6b9fd2467925fd427cd
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-29 10:13:30 +0100

    Disable middleware

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const {signupUser, loginUser} = require('../controllers/userController');
-const requireAuth = require('../middleware/requireAuth')
+//const requireAuth = require('../middleware/requireAuth')
 
-const router = express.Router();
+//const router = express.Router();
 
 router.use(requireAuth)
 
