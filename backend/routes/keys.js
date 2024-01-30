204a1ef2f63d214aec84758edc00e18be268a9bf
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-30 22:29:02 +0100

    re enabled requireauth

--- backend/routes/keys.js
+++ backend/routes/keys.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const {createNewKey, viewAllKeys, getSingleKey, deleteKey, updateKey} = require('../controllers/KeyController');
-//const requireAuth = require('../middleware/requireAuth')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router();
-//router.use(requireAuth)
+router.use(requireAuth)
 
 router.get('/', viewAllKeys);
 
