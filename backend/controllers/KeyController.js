3dbb3c6f48b8da8976cb918c6ab754ef8554bb02
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-31 09:19:15 +0100

    Change in key controller Chenged the req.body (userId, keyNumber, trackUrl)

--- backend/controllers/KeyController.js
+++ backend/controllers/KeyController.js
@@ -30,8 +30,8 @@ const createNewKey = async (req, res)=> {
 
     try{
 
-        const {trackName, trackUrl, user } = req.body
-        const key = await Keys.create({trackName, trackUrl, user})
+        const {userId, keyNumber, trackUrl } = req.body
+        const key = await Keys.create({userId, keyNumber, trackUrl})
         res.status(200).json(key)
     }
 
--- backend/models/KeyModel.js
+++ backend/models/KeyModel.js
@@ -14,7 +14,6 @@ const keySchema = new mongoose.Schema({
         required: true
         }
 
-
 },{ timestamps:true })
 
 
