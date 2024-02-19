bbfa15713e1a29a85d2863992aa9c7dea2db5413
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-19 14:50:37 +0100

    Deleted the CreateKey component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ import Update from './Components/Update';
 import DashBoard from './Components/DashBoard';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
-import CreateKey from './Components/CreateKey';
 import "../src/index.css";
 
 import { UseAuthContext } from "./hooks/UseAuthContext"
@@ -32,7 +31,6 @@ function App() {
               <Route path='/update/:id' element={<Update />}></Route>
               <Route path='/login' element={<Login/>}></Route>
               <Route path='/signup' element={<Signup />}></Route>
-              <Route path='/createkey' element={<CreateKey />}></Route>
               <Route path='/keyboarduser' element={<KeyboardUser />}></Route>
             </Routes>
       <Footer />
