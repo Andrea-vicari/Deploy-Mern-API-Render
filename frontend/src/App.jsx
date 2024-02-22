38a7dd1b5bec95c295a47a5b360a406c7bba2501
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-22 15:00:26 +0100

    Fix user ? <DashBoard />: <Navigate to="/login"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
               <Route path="/register" element={<Signup />}></Route>
               <Route path="/dashboard" element={user ? <DashBoard />: <Navigate to="/login"/>} />
               <Route path='/update/:id' element={<Update />}></Route>
-              <Route path='/login' element={user ? <DashBoard />: <Navigate to="/login"/>}></Route>
+              <Route path='/login' element={user ? <DashBoard />: <Login />}></Route>
               <Route path='/signup' element={<Signup />}></Route>
               <Route path='/keyboarduser' element={<KeyboardUser />}></Route>
             </Routes>
