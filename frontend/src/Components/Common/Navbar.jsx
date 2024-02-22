3d210d6f11cc5a203cc51a142c606aac71460e0a
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-22 18:09:19 +0100

    Deleted the fake logo

--- frontend/src/Components/Common/Navbar.jsx
+++ frontend/src/Components/Common/Navbar.jsx
@@ -19,9 +19,9 @@ function Navbar () {
 
     return(
         <div className="container">
-            <nav className="navbar navbar-dark bg-dark" aria-label="DrumPadmenu">
+            <nav className="navbar navbar-dark bg-black" aria-label="DrumPadmenu">
                 <div className="container-fluid">
-                <a className="navbar-brand" href="#">Never expand</a>
+
                 <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
