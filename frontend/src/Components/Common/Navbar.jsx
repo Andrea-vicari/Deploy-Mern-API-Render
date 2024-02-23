fdf05865238880b8df7773941f2b617aff1e1300
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-23 08:43:30 +0100

    Fixed navbar on going

--- frontend/src/Components/Common/Navbar.jsx
+++ frontend/src/Components/Common/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar () {
         <div className="container">
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">{reactlogogreen}r</a>
+                    <img className="navbar-brand" src={reactlogogreen}></img>
                     <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
